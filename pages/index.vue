2764696061dcaf8fe438d6287530f1aef75f69ad
morikaku <160197250+morikaku@users.noreply.github.com>
2024-04-19 14:30:17 +0900

    Version 0.1

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container">
         <h1>Typing Game</h1>
-        <p v-if="gameStarted">currentWord : {{ currentWord }}</p>
+        <p v-if="gameStarted">CurrentWord : {{ currentWord }}</p>
         <p>Score : {{ score }}</p>
-        <input v-model="userInput" @input="checkInput" v-if="gameStarted" />
-        <button @click="startGame">{{ gameStarted ? 'Reset' : 'Start' }}</button>
+        <!-- <input class="userInput" @input="checkInput" v-if="gameStarted" /> -->
+        <input class="userInput" v-model="userInput" @input="checkInput" ref="userInput">
+
+        <button class="start-stop" @click="startGame">{{ gameStarted ? 'Stop' : 'Start' }}</button>
         <div class="keyboard">
             <div v-for="(row, rowIndex) in keyboard" :key="'row' + rowIndex">
                 <div v-for="key in row" :key="key.letter" :class="{ 'active': key.active }">
@@ -40,13 +42,25 @@ export default {
         window.removeEventListener('keyup', this.keyupHandler);
     },
     methods: {
-        // 省略
-
         startGame() {
-            this.gameStarted = true;
-            this.score = 0;
-            this.userInput = '';
-            this.pickNewWord();
+            if (this.gameStarted) {
+                // Reset the game
+                this.currentWord = '';
+                this.userInput = '';
+                this.score = 0;
+                this.keyboard.forEach(row => row.forEach(key => key.active = false));
+                this.gameStarted = false; // Stop the game
+            } else {
+                // Start the game
+                this.gameStarted = true;
+                this.$nextTick(() => {
+                    this.$refs.userInput.focus(); // Add this line
+                });
+
+                this.score = 0;
+                this.userInput = '';
+                this.pickNewWord();
+            }
         },
         pickNewWord() {
             const randomIndex = Math.floor(Math.random() * this.words.length);
@@ -82,6 +96,17 @@ export default {
     margin: 50px auto 50px;
 }
 
+
+.userInput {
+    width: 200px;
+    margin-bottom: 10px;
+}
+
+.start-stop {
+    width: 100px;
+    margin-bottom: 10px;
+}
+
 .keyboard {
     display: flex;
     flex-direction: column;
@@ -94,9 +119,19 @@ export default {
     padding: 10px;
     margin: 5px;
     border: 1px solid #000;
+    border-radius: 8px;
+}
+
+.keyboard div div {
+    display: block;
+    width: 20px;
+    text-align: center;
+    margin: 5px;
+    border: 1px solid #000;
+    border-radius: 8px;
 }
 
 .keyboard .active {
-    background-color: #f00;
+    background-color: #99f;
 }
 </style>
